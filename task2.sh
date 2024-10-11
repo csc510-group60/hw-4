3c6b0116cbac7af6a6a892e37189f5b83954baa9
Prabhudatta Mishra <84654664+Prabhudatta3004@users.noreply.github.com>
2024-10-10 23:07:37 -0400

    Update task2.sh

--- task2.sh
+++ task2.sh
@@ -1,12 +1,8 @@
-#!/bin/bash
-# List all the files containing "sample" and at least 3 occurrences of "CSC510"
-grep -l 'sample' dataset1/* | while read file; do
-  count=$(grep -o 'CSC510' "$file" | wc -l)
-  if [ "$count" -ge 3 ]; then
-    echo "$file $count"
-  fi
-done | sort -k2,2nr -k1,1 | while read file count; do
-  # Replacing the  "file_" with "filtered_" in the file name and list output
-  newname=$(echo "$file" | sed 's/file_/filtered_/')
-  echo "$newname"
-done
+grep -l "sample" dataset/* | while read file; do
+    count=$(grep -o "CSC510" "$file" | wc -l)
+    size=$(stat -f %z "$file")
+    if [ "$count" -ge 3 ]; then
+        new_name=$(echo "$file" | sed 's/file_/filtered_/')
+        echo "$count $size $new_name"
+    fi
+done | sort -k1,1nr -k2,2nr
