1eb6cb8535fe02b6c61af0942e562de73e535269
Prabhudatta Mishra <84654664+Prabhudatta3004@users.noreply.github.com>
2024-10-23 15:42:38 -0400

    Update task2.sh

--- task2.sh
+++ task2.sh
@@ -1,1 +1,9 @@
-grep -l "sample" ./dataset1/* | xargs grep -o "CSC510" | uniq -c | grep -E "^\s+[3-9][0-9]*|^\s+[1-9][0-9]+\s\w+" | gawk '{print $1, $2}' | sed 's/:CSC510$//' | xargs -I {} sh -c 'file=$(echo "{}" | gawk "{print \$2}"); echo "{}" $(stat -c%s "$file")' | sort -k1,1nr -k3,3nr | sed 's/file/filtered/'
+grep -l "sample" ./dataset1/* \
+| xargs grep -o "CSC510" \
+| uniq -c \
+| grep -E "^\s+[3-9][0-9]*|^\s+[1-9][0-9]+\s\w+" \
+| gawk '{print $1, $2}' \
+| sed 's/:CSC510$//' \
+| xargs -I {} sh -c 'file=$(echo "{}" | gawk "{print \$2}"); echo "{}" $(stat -c%s "$file")' \
+| sort -k1,1nr -k3,3nr \
+| sed 's/file/filtered/'
